df142e27b3c780f250fb1b7db38a8cc74d02b3e0
elsayed-issa <elsayed.sabry.issa@gmail.com>
2023-12-06 15:33:39 -0500

    add datasets to requirements

--- ei/metrics.py
+++ ei/metrics.py
@@ -83,50 +83,50 @@ class SentenceSimilarity:
     pass
 
 
-def word_level_edit_distance(s1, s2):
-    words1, words2 = s1.split(), s2.split()
-    m, n = len(words1), len(words2)
-    dp = np.zeros((m + 1, n + 1), dtype=int)
+# def word_level_edit_distance(s1, s2):
+#     words1, words2 = s1.split(), s2.split()
+#     m, n = len(words1), len(words2)
+#     dp = np.zeros((m + 1, n + 1), dtype=int)
 
-    for i in range(m + 1):
-        for j in range(n + 1):
-            if i == 0:
-                dp[i][j] = j
-            elif j == 0:
-                dp[i][j] = i
-            elif words1[i - 1] == words2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1]
-            else:
-                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+#     for i in range(m + 1):
+#         for j in range(n + 1):
+#             if i == 0:
+#                 dp[i][j] = j
+#             elif j == 0:
+#                 dp[i][j] = i
+#             elif words1[i - 1] == words2[j - 1]:
+#                 dp[i][j] = dp[i - 1][j - 1]
+#             else:
+#                 dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
 
-    visualize_matrix(dp, words1, words2)
-    return dp[m][n]
+#     visualize_matrix(dp, words1, words2)
+#     return dp[m][n]
 
 
-def visualize_matrix(matrix, words1, words2):
-    fig, ax = plt.subplots()
-    cax = ax.matshow(matrix, cmap="viridis_r")
+# def visualize_matrix(matrix, words1, words2):
+#     fig, ax = plt.subplots()
+#     cax = ax.matshow(matrix, cmap="viridis_r")
 
-    ax.set_xticklabels([""] + words1, rotation=45)
-    ax.set_yticklabels([""] + words2)
-    ax.xaxis.set_major_locator(plt.MultipleLocator(1))
-    ax.yaxis.set_major_locator(plt.MultipleLocator(1))
+#     ax.set_xticklabels([""] + words1, rotation=45)
+#     ax.set_yticklabels([""] + words2)
+#     ax.xaxis.set_major_locator(plt.MultipleLocator(1))
+#     ax.yaxis.set_major_locator(plt.MultipleLocator(1))
 
-    for (i, j), z in np.ndenumerate(matrix):
-        ax.text(
-            j,
-            i,
-            f"{z}",
-            ha="center",
-            va="center",
-            bbox=dict(boxstyle="round", facecolor="white", edgecolor="0.3"),
-        )
+#     for (i, j), z in np.ndenumerate(matrix):
+#         ax.text(
+#             j,
+#             i,
+#             f"{z}",
+#             ha="center",
+#             va="center",
+#             bbox=dict(boxstyle="round", facecolor="white", edgecolor="0.3"),
+#         )
 
-    plt.xlabel("Transcript")
-    plt.ylabel("Gold Standard")
-    plt.title("Edit Distance Matrix")
-    plt.colorbar(cax)
-    st.pyplot(fig)
+#     plt.xlabel("Transcript")
+#     plt.ylabel("Gold Standard")
+#     plt.title("Edit Distance Matrix")
+#     plt.colorbar(cax)
+#     st.pyplot(fig)
 
 # if __name__ == "__main__":
 
