716febe31c3df81bb687eb4a4c54a70ae24f93f4
elsayed-issa <elsayed.sabry.issa@gmail.com>
2024-03-25 12:22:35 -0400

    fix __init__.py again

--- ei/__init__.py
+++ ei/__init__.py
@@ -3,9 +3,9 @@ import os
 
 try:
     version_file = os.path.join(os.path.dirname(__file__), "VERSION")
-    print(version_file)
+    #print(version_file)
     with open(version_file, 'r', encoding='utf-8') as version_f:
         __version__ = version_f.read().strip()
-        print(__version__)
+        #print(__version__)
 except Exception:  
     print("Failed to find version")
